61f59808544affc70f19acfa30e4d92203aaf6c5
Adam <adaamr@gmail.com>
2015-09-26 15:10:25 +0100

    Compelted layout, created signup routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ get 'help' => 'static_pages#help'
 get 'about' => 'static_pages#about'
 get 'contact' => 'static_pages#contact'
 get 'signup' => 'users#new'
+get 'login' => "static_pages#login"
 
   
 end
